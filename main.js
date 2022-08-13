f23ee9281f061b536b6144ada3225ec45c24156c
Live Blogger <61611286+livebloggerofficial@users.noreply.github.com>
2022-08-14 03:56:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const tabHeadingAll = document.querySelectorAll(".tabs-container .tab-heading");
+const tabContentAll = document.querySelectorAll(".tabs-container .tab-content");
+
+const removeAllActive = () => {
+  tabContentAll.forEach((c) => {
+    c.classList.remove("active");
+  });
+
+  tabHeadingAll.forEach((h) => {
+    h.classList.remove("active");
+  });
+};
+
+tabHeadingAll.forEach((h, i) => {
+  h.addEventListener("click", () => {
+    removeAllActive();
+    tabContentAll[i].classList.add("active");
+    h.classList.add("active");
+  });
+});
